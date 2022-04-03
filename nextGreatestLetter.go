2c05558e22d34fedeafece70e52e523f436609a7
phoenix <442770018@qq.com>
2022-04-03 09:44:26 +0800

    leetcode-golang 提交

---
+++ nextGreatestLetter.go
@@ -0,0 +1,11 @@
+package main
+
+func nextGreatestLetter(letters []byte, target byte) byte {
+	for i:=0;i<len(letters);i++{
+		if letters[i] > target{
+			return letters[i]
+		}
+		continue
+	}
+	return letters[0]
+}
